5f96334fdf087e5e9af765c51350baafd9e082da
itstime <itstime0809@gmail.com>
2023-06-21 14:16:40 +0900

    Front: web screen implementation

---
+++ children-playground-project/ChildrenFrontEnd/Web-init/init.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('moveBtn');
+
+// 버튼 클릭 이벤트 처리
+button.addEventListener("click", function() {
+  // 다른 폴더의 example.html로 이동
+   window.location.href="../Web-main/main.html"
+});
